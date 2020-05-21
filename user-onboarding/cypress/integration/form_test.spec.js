c3e2c781c2e415a979ec1598083abe32a88c48dc
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-05-21 16:34:17 -0500

    tests for able to visit url, enter name, email, button disabled passed

---
+++ user-onboarding/cypress/integration/form_test.spec.js
@@ -0,0 +1,21 @@
+describe("Form inputs", () => {
+  it("can navigate to the site", () => {
+    cy.visit("http://localhost:3000");
+    cy.url().should("include", "localhost");
+  });
+
+  it("button is disabled", () => {
+    cy.get("button").should("be.disabled");
+  });
+
+  it("can type a username", () => {
+    cy.get('input[name="name"]')
+      .type("Lady Gaga")
+      .should("have.value", "Lady Gaga");
+  });
+  it("can type an email", () => {
+    cy.get('input[name="email"]')
+      .type("vickie@vickie.com")
+      .should("have.value", "vickie@vickie.com");
+  });
+});
